800defbc05192cfa4fe8fc9f64f4eecc47ac64a5
ahmad <b-9k@outlook.com>
2024-09-14 14:34:16 -0400

    added expenses manager with option to list expenses

--- src/coinmaster.sh
+++ src/coinmaster.sh
@@ -5,6 +5,18 @@ source "$SRC_HOME/src/shared-functions.sh"
 export coinmaster_location='/home/ahmad/data/finances/coinmaster.sqlite' #FIXME: move to .env
 export backup_location='/home/ahmad/data/finances/coinmaster.dump'
 export transactions_manager="$SRC_HOME/src/coinmaster-transaction.sh"
+export expenses_manager="$SRC_HOME/src/coinmaster-expense.sh"
+
+print_guidance() {
+	echo 'coinmaster:
+    cli > starts sqlite3 shell
+    open > opens database in sqlitebrowser
+    connect > invoke connection  manager
+    transac > invoke transaction manager
+    expense > invoke expenses manager
+    new transac > inserts a transaction in coinmaster
+    dump > dump database'
+}
 
 main() {
 
@@ -15,6 +27,7 @@ main() {
         gui | open) open_sqlitebrowser ;;
         new) shift ; route_new "$@" ;;
         tx | transac*) shift ; "$transactions_manager" "$@" ;;
+        xp | *xpen*  ) shift ; "$expenses_manager" "$@" ;;
         dump | backup) dump_database ;;
         *) echo 'nothing happened...' ;;
 
@@ -32,16 +45,6 @@ EOF
 
 echo "coinmaster.sqlite dumped to /home/ahmad/data/finances/coinmaster.dump (hardcoded)"
 
-}
-
-print_guidance() {
-	echo 'coinmaster:
-    cli > starts sqlite3 shell
-    open > opens database in sqlitebrowser
-    connect > invoke connection  manager
-    transac > invoke transaction manager
-    new transac > inserts a transaction in coinmaster
-    dump > dump database'
 }
 
 open_sqlitebrowser() {
